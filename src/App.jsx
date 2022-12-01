b0aa647645c04941de272165976043d58b237291
nicolasteofilo <nicolasteofilodecastro@gmail.com>
2022-12-01 12:09:40 -0300

    chore: create initial structure

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100vh",
+        backgroundColor: "#242526",
+        color: "#E6E6E6",
+        fontFamily: "monospace",
+      }}
+    >
+      <h1 style={{ fontSize: "24px" }}>Custom Hooks</h1>
+      <p style={{ display: "block", marginTop: "10px" }}>
+        Deixa teu like e inscreva-se no canal :)
+      </p>
+    </div>
+  );
+}
+
+export default App;
